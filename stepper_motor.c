0bd992344801c775f6856b9d08f55b612fb2cc5d
Sai Nikhita <sainikhitanayani@gmail.com>
2019-12-01 19:50:58 +0530

    Stepper motor

---
+++ stepper_motor.c
@@ -0,0 +1,54 @@
+#include<reg51.h> //include at89c51 microcontoller header file
+
+sbit sw=P0^0;   //connect p0.0 to switch
+#define motor P2 //connect lower nibble of port2 to stepper motor
+
+void delay_ms(unsigned int);
+
+bit dir=0; //define direction flag
+void main(void)
+{
+while(1) //infinite loop
+{
+	do
+	{       
+		if(dir==0) //if dir==0, motor rotates in counter clockwise direction
+		{
+      	motor=0x06;   //for CCW direction "C639"
+			  delay_ms(1);
+	      motor=0x0a;
+			  delay_ms(1);
+	      motor=0x09;
+			  delay_ms(1);
+       	motor=0x05;
+			  delay_ms(1);
+		}else	
+		if(dir==1) //if dir==1, motor rotates in clockwise direction
+		{
+       	motor=0x05;   //for CW direction "936C"
+			  delay_ms(1);
+	      motor=0x09;
+			  delay_ms(1);
+	      motor=0x0a;
+			  delay_ms(1);
+       	motor=0x06;
+			  delay_ms(1);
+		}	
+	}while(sw==1); //wait till switch is pressed
+	delay_ms(200); //switch debounce delay 200 milli seconds
+	dir=~dir; // if switch pressed, toggle direction flag
+}	
+}
+
+//generates delay in milli seconds
+void delay_ms(unsigned int i)
+{
+unsigned int j;
+	while(i-->0)
+	{
+		for(j=0;j<500;j++)
+		{
+			;
+		}
+	}
+}
