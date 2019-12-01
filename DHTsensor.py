b985fe67dc1e403b6e8d9302503fd286bccf9fb6
Sai Nikhita <sainikhitanayani@gmail.com>
2019-12-01 20:06:00 +0530

    Update DHTsensor.py

--- DHTsensor.py
+++ DHTsensor.py
@@ -3,3 +3,5 @@ import Adafruit_DHT
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 27)  # GPIO27 (BCM notation)
     print humidity, temperature
+
+#http://invent.module143.com/daskal_tutorial/raspberry-pi-3-gpio-dht11-digital-temperature-humidity-sensor/
